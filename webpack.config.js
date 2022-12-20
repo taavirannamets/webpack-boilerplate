61ec3a87d2d734ed039f9922e028662194ca8c2f
taavirannamets <taavirannamets@gmail.com>
2022-12-20 17:15:17 +0200

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist/assets'),
-		filename: 'bundle.js',
+	mode: 'development',
+	entry: {
+		bundle: path.resolve(__dirname, 'src/index.js'),
 	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name][contenthash].js',
+		clean: true,
+		assetModuleFilename: '[name][ext]',
+	},
+	devtool: 'source-map',
 	devServer: {
 		static: {
-			directory: path.join(__dirname, 'dist'),
+			directory: path.resolve(__dirname, 'dist'),
 		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
 	},
 	module: {
 		rules: [
@@ -23,6 +35,17 @@ module.exports = {
 					},
 				},
 			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
 		],
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Lingobot',
+			filename: 'index.html',
+			template: 'src/template.html',
+		}),
+	],
 }
